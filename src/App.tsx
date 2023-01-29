482afaeda2c1bce9a91f0913f286f399a2949c43
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-01-29 03:30:22 +0300

    refactor: use useSWR as the fetch hook directly
    remove useFetch as useSWR includes its own loading status property named isLoading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { Button } from '@mui/material';
+import useSWR from 'swr';
 import { toggleStatus } from './store/reducers';
 import { useAppSelector, useAppDispatch } from './store/hooks';
-import { useFetch } from './hooks/useFetch';
+import { fetcher } from './utils/fetcher';
 
 interface Placeholder {
   userId: number;
@@ -15,7 +16,7 @@ function App(): JSX.Element {
   const { exists } = useAppSelector((state) => state.example);
   const dispatch = useAppDispatch();
 
-  const { data, loading } = useFetch<Placeholder>('https://jsonplaceholder.typicode.com/todos/1');
+  const { data, isLoading } = useSWR<Placeholder>('https://jsonplaceholder.typicode.com/todos/1', fetcher);
 
   useEffect(() => {
     console.log('exists: ', exists);
@@ -34,7 +35,7 @@ function App(): JSX.Element {
       <h1>Hello</h1>
       <Button variant="outlined">Material UI button</Button>
       <Button variant="contained">Another button</Button>
-      <p>{loading ? 'Loading...' : data && JSON.stringify(data)}</p>
+      <p>{isLoading ? 'Loading...' : data && JSON.stringify(data)}</p>
     </div>
   );
 }
