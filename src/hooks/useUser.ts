b2ec097510343ade82de1d92a8ff64471eb86490
MetalKnock <102481723+MetalKnock@users.noreply.github.com>
2023-02-04 00:50:55 +0700

    fix: change useUser hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -4,11 +4,12 @@ import { IUser } from '../types/data';
 import { fetcher } from '../utils/fetcher';
 
 export default function useUser(id: number) {
-  const { data, error, isLoading } = useSWR<IUser, Error>(`${BASE_URL}${PATH.users}/${id}`, fetcher);
+  const { data, error, isLoading, mutate } = useSWR<IUser, Error>(`${BASE_URL}${PATH.users}/${id}`, fetcher);
 
   return {
     user: data,
     isLoading,
     isError: error,
+    mutate
   };
 }
