0d8590dfef5db40799a2fe372f96498d9ab22c1e
MetalKnock <102481723+MetalKnock@users.noreply.github.com>
2023-02-03 21:56:40 +0700

    fix: change type for data

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,10 +1,10 @@
 import useSWR from 'swr';
 import { BASE_URL, PATH } from '../constants';
-import { IUser } from '../types/data';
+import { IPost } from '../types/data';
 import { fetcher } from '../utils/fetcher';
 
 export default function usePost(id: number) {
-  const { data, error, isLoading } = useSWR<IUser, Error>(`${BASE_URL}${PATH.posts}/${id}`, fetcher);
+  const { data, error, isLoading } = useSWR<IPost, Error>(`${BASE_URL}${PATH.posts}/${id}`, fetcher);
 
   return {
     post: data,
