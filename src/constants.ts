be1b09531cfb70daa4def4e8f2b7f09c900a4f26
Dmitry Ivanov <dimannisik1@gmail.com>
2023-02-03 19:58:19 +0700

    feat: add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
-export {};
+const BASE_URL = 'http://localhost:3000/api';
+
+const enum PATH {
+  posts = '/posts',
+  users = '/users',
+  comments = 'comments',
+  auth = '/posts-auth',
+}
+
+export { BASE_URL, PATH };
