f9dbba943b921405fb091fa222bef9169e6b8ece
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-07 02:29:04 +0300

    docs: remove redux-toolkit example

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ import Login from './pages/Login';
 import Registration from './pages/Registration';
 
 function App() {
-  /*
-   * Store can be accessed and modified this way.
-   * It automatically subscribes to updates, meaning the updates are seen from the component body / useEffect.
-   */
-  // const { exists } = useAppSelector((state) => state.reducers);
-  // const dispatch = useAppDispatch();
   return (
     <>
       <CssBaseline />
