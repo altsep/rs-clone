f2d59b893cae7401b9f6f565f9621bfaacd77017
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-06 03:51:30 +0300

    refactor: comment out example in App, remove the fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import useSWR from 'swr';
 import { Routes, Route } from 'react-router-dom';
-import { useAppSelector, useAppDispatch } from './store/hooks';
-import { fetcher } from './utils/fetcher';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 
-interface Placeholder {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
 function App() {
-  const { exists } = useAppSelector((state) => state.reducers);
-  const dispatch = useAppDispatch();
-
-  const { data, isLoading } = useSWR<Placeholder>('https://jsonplaceholder.typicode.com/todos/1', fetcher);
-
+  /*
+   * Store can be accessed and modified this way.
+   * It automatically subscribes to updates, meaning the updates are seen from the component body / useEffect.
+   */
+  // const { exists } = useAppSelector((state) => state.reducers);
+  // const dispatch = useAppDispatch();
   return (
     <>
       <header />
