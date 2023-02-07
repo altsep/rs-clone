d3c75491d5e6243e35f69db98bde8635bd2257df
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-07 04:04:15 +0300

    build: remove 'vite-plugin-checker'

--- root/vite.config.ts
+++ root/vite.config.ts
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
   },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    {
-      ...checker({
-        typescript: true,
-        eslint: {
-          lintCommand: 'eslint ..',
-          dev: {
-            logLevel: ['error'],
-          },
-        },
-      }),
-      apply: 'serve',
-    },
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
