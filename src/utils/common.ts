61d64342862e6cf1019f208b51964f94e88be2fb
Dmitry Ivanov <dimannisik1@gmail.com>
2023-02-08 19:03:36 +0700

    refactor: remove unnecessary code

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,3 @@
 const getFirstLetter = (str: string): string => str.slice(0, 1);
 
-const getNewArrayFilledWithSequence = (start: number, end: number): number[] =>
-  new Array(end - start + 1).fill(0).map((_, i) => i);
-
-export { getFirstLetter, getNewArrayFilledWithSequence };
+export { getFirstLetter };
