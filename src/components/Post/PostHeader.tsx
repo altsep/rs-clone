4d799703fc2fb1cdc1e4af91ded4052e9dee37f2
Dmitry Ivanov <dimannisik1@gmail.com>
2023-02-15 21:37:44 +0700

    fix: fix deleting post

--- src/components/Post/PostHeader.tsx
+++ src/components/Post/PostHeader.tsx
@@ -13,6 +13,7 @@ import { removePostInState } from '../../store/reducers/postsState';
 import { updateUser } from '../../api/usersApi';
 import { TUpdateUserArg } from '../../types/usersApi';
 import { filterCommentsByPostId } from '../../store/reducers/commentsState';
+import { updateUserInState } from '../../store/reducers/usersState';
 
 interface IPostHeaderProps {
   postData: IPost;
@@ -23,7 +24,7 @@ export default function PostHeader({ postData, setIsEdit }: IPostHeaderProps) {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   const dispatch = useAppDispatch();
-  const { idCurrentProfile, users, idAuthorizedUser } = useAppSelector((state) => state.users);
+  const { idCurrentProfile, users, idAuthorizedUser, currentProfile } = useAppSelector((state) => state.users);
 
   const open = Boolean(anchorEl);
   const currentUser = users.find((user) => user.id === postData.userId);
@@ -43,16 +44,21 @@ export default function PostHeader({ postData, setIsEdit }: IPostHeaderProps) {
   };
 
   const handleClickDeletePost = async (): Promise<void> => {
-    if (currentUser) {
-      setAnchorEl(null);
-      await triggerRemovePost();
-      const argUpdateUser: TUpdateUserArg = {
-        postsIds: currentUser.postsIds ? currentUser.postsIds.filter((postId) => postId !== postData.id) : [],
-      };
-      await triggerUpdateUser(argUpdateUser);
-      dispatch(removePostInState(postData.id));
-      dispatch(filterCommentsByPostId(postData.id));
+    console.log(currentProfile?.postsIds);
+    setAnchorEl(null);
+    await triggerRemovePost();
+    const argUpdateUser: TUpdateUserArg = {
+      postsIds:
+        currentProfile && currentProfile.postsIds
+          ? currentProfile.postsIds.filter((postId) => postId !== postData.id)
+          : [],
+    };
+    const responseData = await triggerUpdateUser(argUpdateUser);
+    if (responseData) {
+      dispatch(updateUserInState(responseData));
     }
+    dispatch(removePostInState(postData.id));
+    dispatch(filterCommentsByPostId(postData.id));
   };
 
   const handleClickEditPost = (): void => {
