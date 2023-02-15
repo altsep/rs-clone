021a6b9e1dea697c02b098bdf130022895426760
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-15 20:18:31 +0300

    build: set serializableCheck in configureStore to false
    this allows to store class instances in a redux store, although it is not recommended

--- src/store/index.ts
+++ src/store/index.ts
@@ -20,6 +20,10 @@ const combinedReducers = combineReducers({
 
 const store = configureStore({
   reducer: combinedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
