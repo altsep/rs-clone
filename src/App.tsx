b93bad53d29bc5c0a737a4d41ac719dff1e91a14
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-19 12:31:06 +0300

    refactor: create a separate hook from checkAuth and the connected useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { useEffect } from 'react';
-import useSWRMutation from 'swr/mutation';
-import { useLocation } from 'react-router-dom';
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { darkTheme } from './themes/darkTheme';
 import { lightTheme } from './themes/lightTheme';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import Header from './components/Header/Header';
-import { ApiPath, API_BASE_URL, KEY_LOCAL_STORAGE, LSKeys } from './constants';
-import { refreshToken } from './api/usersApi';
-import { ILogin } from './types/data';
-import { getToken, setToken } from './utils/common';
-import { setAuth, setAuthError, setLoading } from './store/reducers/authSlice';
-import { setUser, usersLoadingSuccess } from './store/reducers/usersState';
+import { usersLoadingSuccess } from './store/reducers/usersState';
 import usePosts from './hooks/usePosts';
 import useUsers from './hooks/useUsers';
 import { postsLoadingSuccess } from './store/reducers/postsState';
@@ -20,48 +13,17 @@ import { commentsLoadingSuccess } from './store/reducers/commentsState';
 import Footer from './components/Footer/Footer';
 import useMessagesWs from './hooks/useMessagesWs';
 import Routes from './components/Routes';
+import useAuth from './hooks/useAuth';
 
 function App() {
   const dispatch = useAppDispatch();
-  const location = useLocation();
   const theme = useAppSelector((state) => state.theme.mode);
 
   const { users, isLoadingUsers, isValidatingUsers } = useUsers();
   const { posts, isLoadingPosts, isValidatingPosts } = usePosts();
   const { comments, isLoadingComments, isValidatingComments } = useComments();
 
-  const { trigger } = useSWRMutation(`${API_BASE_URL}${ApiPath.refresh}`, refreshToken);
-
-  const checkAuth = async (): Promise<void> => {
-    dispatch(setAuth(false));
-    dispatch(setLoading(true));
-    try {
-      const res: Response | undefined = await trigger();
-      if (res?.ok) {
-        const resData = (await res?.json()) as ILogin;
-        const { accessToken, user } = resData;
-        dispatch(setAuth(true));
-        setToken(accessToken);
-        dispatch(setUser(user));
-      } else {
-        throw new Error('Authorization error. Re-login required');
-      }
-    } catch {
-      dispatch(setAuthError(true));
-    } finally {
-      dispatch(setLoading(false));
-    }
-  };
-
-  useEffect(() => {
-    const accessToken = getToken();
-
-    if (accessToken) {
-      checkAuth().catch((err: Error): Error => err);
-    }
-
-    localStorage.setItem(`${LSKeys.path}_${KEY_LOCAL_STORAGE}`, location.pathname);
-  }, []);
+  useAuth();
 
   useMessagesWs();
 
