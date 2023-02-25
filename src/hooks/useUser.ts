1907e3b5758ecb0cda44d9d82647a3fdac00930f
Dmitry Ivanov <dimannisik1@gmail.com>
2023-02-26 04:14:44 +0700

    fix: remove polling for an authorized user

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,10 +1,13 @@
 import useSWR from 'swr';
+import { useAppSelector } from './redux';
 import { API_BASE_URL, ApiPath } from '../constants';
 import { IUser } from '../types/data';
 import { fetcher } from '../utils/fetcher';
 
 export default function useUser(id: number) {
-  const { data } = useSWR<IUser>(`${API_BASE_URL}${ApiPath.users}/id${id}`, fetcher, {
+  const { idAuthorizedUser } = useAppSelector((state) => state.users);
+
+  const { data } = useSWR<IUser>(id !== idAuthorizedUser && `${API_BASE_URL}${ApiPath.users}/id${id}`, fetcher, {
     refreshInterval: 10000,
   });
 
