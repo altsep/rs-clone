7bbc5dbc8f291c73623caf6b6beaa60c12a81834
krevetka87 <bonanast4sia@yandex.ru>
2023-02-27 17:10:14 +0300

    feat: add styles to settings page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Typography, useMediaQuery } from '@mui/material';
+import { Box, Container, Divider, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import FooterGithubLinks from './FooterGithubLinks';
 import FooterRSLink from './FooterRSLink';
