77e7dac855d822a31d916366fc29b4d4f01f652c
krevetka87 <bonanast4sia@yandex.ru>
2023-02-27 22:40:08 +0300

    feat: add styles to 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,31 @@
+import { Button, Stack, Typography } from '@mui/material';
+import WestIcon from '@mui/icons-material/West';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../hooks/redux';
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { isAuth } = useAppSelector((state) => state.auth);
+  const { idAuthorizedUser, authorizedUser } = useAppSelector((state) => state.users);
+
+  const onClick = (): void => {
+    navigate(!isAuth ? '/' : `${authorizedUser?.alias ? `/${authorizedUser?.alias}` : `/id${idAuthorizedUser}`}`);
+  };
+
+  return (
+    <Stack sx={{ mt: '5vh', mb: '5vh', pr: '10px', pl: '10px' }}>
+      <Typography variant="h4" sx={{ mb: '10px' }}>
+        Oops....
+      </Typography>
+      <Typography variant="subtitle1" sx={{ fontWeight: 700, mb: '10px' }}>
+        {t('404.title')}
+      </Typography>
+      <Typography sx={{ mb: '40px' }}>{t('404.text')}</Typography>
+      <Button variant="contained" startIcon={<WestIcon />} sx={{ alignSelf: 'flex-start' }} onClick={onClick}>
+        {t('404.link')}
+      </Button>
+    </Stack>
+  );
 }
